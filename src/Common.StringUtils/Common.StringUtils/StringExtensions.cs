7950bf52bd2048bbfbb5cbfbe7dfb8af69d44012
semashkin <semashkin.vg@gmail.com>
2020-05-10 15:15:04 +0400

    initial commit

---
+++ src/Common.StringUtils/Common.StringUtils/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ItWasMe.Common.StringUtils
+{
+    public static class StringExtensions
+    {
+        public static string NormalizeToForm(this string input, NormalizationForm normalizationForm = NormalizationForm.FormD)
+        {
+            return string.Concat(input.Normalize(NormalizationForm.FormD).Where(
+                c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark));
+        }
+    }
+}
